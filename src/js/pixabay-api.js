bbad58f99888e5a2ca158f46ef88a53a3f736a0a
AlexBoyko5 <144361285+AlexBoyko5@users.noreply.github.com>
2024-02-20 19:40:17 +0100

    commit#2
    axios works but with mistake  appearing "Please enter a search query"

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,13 +2,36 @@
 import iziToast from "izitoast";
 // Додатковий імпорт стилів
 import "izitoast/dist/css/iziToast.min.css";
+import axios from 'axios';
 import { createGalleryMarkup } from './render-functions.js';
 import { lightbox } from '../main.js';
 
 const API_KEY = '42334631-07f239856d3b6a49db441bfb9';
-export function fetchImages(query) {
+export async function fetchImages(query) {
     const loader = document.querySelector('.loader');
     loader.style.display = 'block';
+    try {
+        const response = await axios.get(`https://pixabay.com/api/?key=${API_KEY}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true`);
+        console.log(response.data);
+        loader.style.display = 'none';
+        if (response.data.hits.length === 0) {
+            iziToast.info({
+                title: 'Info',
+                message: 'Sorry, there are no images matching your search query. Please try again!'
+            });
+        } else {
+            createGalleryMarkup(response.data.hits);
+            lightbox.refresh();
+        }
+    } catch (error) {
+        console.error('Error fetching images:', error);
+        loader.style.display = 'none';
+        iziToast.error({
+            title: 'Error',
+            message: 'Failed to fetch images. Please try again later.'
+        });
+    }
+    //^==================================================================================================
     fetch(`https://pixabay.com/api/?key=${API_KEY}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true`)
         .then(response => {
             if (!response.ok) {
